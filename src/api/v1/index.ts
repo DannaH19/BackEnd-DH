df3aab7d1e4adb5ac8892c4f21f2b8a1a28a878b
DannaH19 <dlherrerar@itc.edu.co>
2026-05-14 18:53:48 -0500

    feat: add users module with CRUD

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import UsersRouter from '../../modules/users/users.routes';
 import AuthRouter from '../../modules/auth/auth.routes';
 import bookRoutes from '../../modules/library/book.routes';
-
+import categoryRoutes from '../../modules/category/category.routes';
+import authorRoutes from '../../modules/author/author.routes';
+import reviewRoutes from '../../modules/reviews/review.routes';
 
 const router = Router();
 
 router.use('/users', UsersRouter);
 router.use('/auth', AuthRouter);
 router.use('/books', bookRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/authors', authorRoutes);
+router.use('/reviews', reviewRoutes);
 
 export default router;
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { HttpError } from "../libs/http-error";
 
 export const errorMiddleware = (
   error: Error,
@@ -6,7 +7,10 @@ export const errorMiddleware = (
   res: Response,
   _next: NextFunction
 ) => {
-  return res.status(500).json({
+  const status = error instanceof HttpError ? (error as HttpError).statusCode : 500;
+  
+  return res.status(status).json({
+    ok: false,
     message: error.message || 'Error interno del servidor',
   });
 };
